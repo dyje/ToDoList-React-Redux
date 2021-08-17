bb375c8a495f4191efa46c26b0902da8a7bf49bf
dyje <dyje@oocl.com>
2021-08-17 21:00:48 +0800

    [ToDoList][Jesse] feat: Created components for ToDoGroup and ToDoList

---
+++ my-app/src/components/ToDoGroup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoItem from './ToDoItem'
+
+function TodoGroup() {
+    return (
+        <div>
+            TodoGroup
+            <TodoItem/>
+        </div>
+    )
+}
+
+export default TodoGroup
