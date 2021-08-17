6a22557998ac9fecaf4e1c40fb6cd52bccaee82f
dyje <dyje@oocl.com>
2021-08-17 21:22:56 +0800

    [ToDoList][Jesse] feat: add common constants and utils

---
+++ my-app/src/common/constants/constants.js
@@ -0,0 +1,12 @@
+export const initialTodoList = [
+    {
+        id: "1",
+        text: "Todo list # 1",
+        done: false
+    },
+    {
+        id: "2",
+        text: "Todo list # 2",
+        done: false
+    },
+]; 
