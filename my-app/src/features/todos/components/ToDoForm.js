edbcf3f88ade99746e37d50d0c05fa06ad67d035
dyje <dyje@oocl.com>
2021-08-18 19:02:07 +0800

    [ToDoList][Jesse] feat: implementation of calling post API

--- my-app/src/features/todos/components/ToDoForm.js
+++ my-app/src/features/todos/components/ToDoForm.js
@@ -1,35 +1,40 @@
 import React, {useState} from 'react';
 import {useDispatch} from 'react-redux';
+import { createTodo } from '../../apis/todos';
 import {AddToDo} from "..//reducers/ToDosSlice";
+import {Button} from 'antd';
 
 function ToDoForm(){
     const [inputText, setText] = useState("");
-    const dispatch = useDispatch();
-
+    const dispatch = useDispatch(); 
+    
     function handleInputTextChange(event){
         setText(event.target.value);
         //console.log(event.target.value);
     }
 
     function handleInputTextAdd(){
+        createTodo(inputText).then((response)=> {
+            dispatch(AddToDo(response.data));
+        })
         //console.log("Input text to be added: ", inputText);
-        dispatch(AddToDo(inputText));
         setText("");
     }
 
     return (
         <div>
+            <div>
             <input
                 type = "text"
-                placeholder = "Input a new ToDo item"
+                placeholder = "Input new ToDo item"
                 value = {inputText}
                 onChange = {handleInputTextChange}
             ></input>
-            <button
-                onClick= {handleInputTextAdd}
-            >Add</button>
+            <Button type="primary" onClick= {handleInputTextAdd}>ADD</Button>
+            </div>
         </div>
     )
 }
 
 export default ToDoForm
+
