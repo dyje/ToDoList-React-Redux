bfad8fb8d47ce02924551bb5cff25dc789b8139d
dyje <dyje@oocl.com>
2021-08-18 14:42:58 +0800

    [ToDoList][Jesse] feat: implementation of React router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import TodoGroup from './features/todos/components/ToDoGroup';
 import TodoList from './features/todos/components/ToDoList';
+import DoneList from './features/todos/components/DoneList';
+import NotFoundPage from './features/notFound/NotFoundPage';
+
+import {Route, Link, BrowserRouter, Switch, HashRouter} from 'react-router-dom';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <TodoList></TodoList>
-    </div>
+    <React.Fragment>
+        <header></header>
+      <BrowserRouter>
+        <ul>
+          <Link to="/">go to the todo list page</Link>
+          <Link to="/done">go to the done list page</Link>
+        </ul>
+        <Switch>
+          <Route exact path= "/" component={TodoList}></Route>
+          <Route exact path= "/done" component={DoneList}></Route>
+          <Route exact path= "*" component={NotFoundPage}></Route>       
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
---
+++ my-app/src/features/notFound/NotFoundPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function NotFoundPage() {
+
+
+    return (
+        <div>
+            <h1>this the is the NOT FOUND PAGE.</h1>
+        </div>
+    )
+}
+
+export default NotFoundPage
