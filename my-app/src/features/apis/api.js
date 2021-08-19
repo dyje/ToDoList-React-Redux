53e9ead78c48ecfa541b6ecf79c7e423106e50ab
dyje <dyje@oocl.com>
2021-08-19 18:58:51 +0800

    [ToDoList_Frontend_and_Backend][Jesse] feat: added the baseURL of backend for connection

--- my-app/src/features/apis/api.js
+++ my-app/src/features/apis/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://611cc14ba18e850017decc23.mockapi.io/",
+    //baseURL: "https://611cc14ba18e850017decc23.mockapi.io/",
+    baseURL: "http://localhost:8090",
 });
 
 export default api;
