f14766b39f5ef02c70a76fee70790ce95d1f1888
dyje <dyje@oocl.com>
2021-08-19 18:39:03 +0800

    [ToDoList_Frontend_and_Backend][Jesse] refactor: refactored codes to display ToDo items when accessing exact path

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,14 +5,29 @@ import DoneList from './features/todos/components/DoneList';
 import NotFoundPage from './features/notFound/NotFoundPage';
 import {Button} from 'antd';
 import {useEffect} from 'react';
+import {AddToDos} from './features/todos/reducers/ToDosSlice';
+import { getTodos } from './features/apis/todos';
+import { useDispatch } from 'react-redux';
 
 import {Route, Link, BrowserRouter, Switch, HashRouter} from 'react-router-dom';
 import React from 'react';
 
 function App() {
+  const dispatch = useDispatch();
   useEffect(() => {
     document.title = "My To Do List";
+
+    getTodos().then((response) => {
+      dispatch(AddToDos(response.data));
+  })
   }, []);
+
+//   useEffect (() => {
+//     getTodos().then((response) => {
+//         dispatch(AddToDos(response.data));
+//     })
+// }, [])
+
   return (
     <React.Fragment>
         <header></header>
